17265d2fa140c46bdda6e7b3d19e034c3c96695b
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-04-05 19:09:51 +0530

    Updated the variable names

--- battleshipLoader.py
+++ battleshipLoader.py
@@ -4,12 +4,13 @@ COL_SIZE = 10
 ROW_SIZE = 10
 NO_OF_SHIPS = 3
 SHIP_SIZE = 4
-h = [] # total number of hits
-h.append(0)
-c = [] # total number of chances
-c.append(0)
+hits = [] # total number of hits
+hits.append(0)
+chances = [] # total number of chances
+chances.append(0)
+full_line = 190
 st = "Welcome to battleship game"
-ste = st.center(190, "-")
+ste = st.center(full_line, "-")
 print(ste)
 grid = [[0] * COL_SIZE for j in range(ROW_SIZE)]
 board = [["*"] * COL_SIZE for j in range(ROW_SIZE)]
@@ -34,16 +35,16 @@ class Ship:
 
 
 def print_board():
-    symb = "*"
-    for i in range(190):
+    border = 64
+    pos_indicator = 10
+    for i in range(full_line):
         print("-", end = "")
     print()
-    for i in range(41):
+    for i in range(border):
         print("_", end = "")
     print()
     k = 0
-    l = 0
-    for i in range(10): 
+    for i in range(pos_indicator): 
         print("   |" , i , end = "")
     print("   |")
     for i in range(len(board)):
@@ -52,26 +53,27 @@ def print_board():
             print("  |  " + board[i][j] , end = "")
         print("  |")
         k += 1
-    for i in range(41):
+    for i in range(border):
         print("_", end = "")
     print()
 
 
 def check_input(a):
-    c[0] += 1
+    max_hits = 12
+    chances[0] += 1
     for i in range(len(grid)):
         if(grid[a[0]][a[1]] == 1):
             print("HIT!!!") 
             board[a[0]][a[1]] = "#"
             print_board()
-            h[0] += 1
+            hits[0] += 1
             break
         else:
             print("MISS...")
             board[a[0]][a[1]] = "."
             print_board()
             break
-    if(h[0] == 12):
+    if(hits[0] == max_hits):
         fin()
     else:
         get_input()
@@ -79,10 +81,10 @@ def check_input(a):
     
 def get_input():
     a = []
-    print("Please enter coordinates  ")
-    for q in range(2):
-        b = int(input())
-        a.append(b)
+    inp_row = int(input("Enter the row coordinate:  "))
+    a.append(inp_row)
+    inp_col = int(input("Enter the column coordinate:  "))
+    a.append(inp_col)
     check_input(a)
 
 
@@ -112,12 +114,12 @@ def get_location():
 
 
 def fin():
-    for i in range(190):
+    for i in range(full_line):
         print("-", end = "")
     print()
     print("Game Over")
-    print("Total Number of chances = ", c[0])
-    for i in range(190):
+    print("Total Number of chances = ", chances[0])
+    for i in range(full_line):
         print("-", end = "")
     print()
 
