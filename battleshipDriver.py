010fcd3d37d82fe694efa62fa4f4343b9c058296
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-04-12 23:32:34 +0530

    created table and inserted values

--- battleshipDriver.py
+++ battleshipDriver.py
@@ -1,8 +1,21 @@
+from itertools import cycle
+from battleShipUser import *
 import sys
 import os
+import mysql.connector
+import time
 
-from battleShipUser import *
-from itertools import cycle
+start_time = time.time()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="RamuSQL#123kaka",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
 
 COL_SIZE = 10
 ROW_SIZE = 10
@@ -23,6 +36,17 @@ def fin(chances, player):
     print("Game Over")
     print("Player '{0}' won!".format(player + 1))
     print("Total Number of chances: ", chances)
+    print("Total time taken  = ", (time.time() - start_time), " seconds")
+    # for j in range(NO_OF_USERS):
+    #     user_name.append(input("Enter name of user "))
+    #     for user in user_name:
+    #         sql = "INSERT INTO players (name,score) VALUES (%s)(%s)"
+    #         val = [(user, chances)]
+    #     mycursor.execute(sql, val)
+    sql1 = "INSERT INTO players (score) VALUES (%s)"
+    val1 = [(chances)]
+    mycursor.execute(sql1, val1)
+    mydb.commit()
     for i in range(FULL_LINE):
         print("-", end="")
     print()
@@ -33,6 +57,8 @@ global NO_OF_USERS
 
 
 def intro():
+    # mycursor = mydb.cursor()
+    # user_name = ""
     Board = [["*"] * COL_SIZE for j in range(ROW_SIZE)]
     st = "Welcome to battleship game"
     ste = st.center(FULL_LINE, "-")
@@ -56,9 +82,19 @@ def intro():
     print(" 'H' indicates hits")
     print(" '-' indicates misses")
     print("The ships are either horizontal or vertical,\nThey are not diagonal.")
+    for j in range(NO_OF_USERS):
+        user_name.append(input("Enter name of user "))
+        for user in user_name:
+            sql = "INSERT INTO players (name) VALUES (%s)"
+            val = [(user)]
+        mycursor.execute(sql, val)
+    # mydb.commit()
+    # mycursor.execute(sql, val)
+    # mydb.commit()
     print("Sink Them All!")
     print("Good Luck!")
     print_board(Board)
+
     return [NO_OF_SHIPS, NO_OF_USERS]
 
 
@@ -66,24 +102,29 @@ def intro():
 ship_list = []
 chances = 0
 user_list = []
+user_name = []
 [NO_OF_SHIPS, NO_OF_USERS] = intro()
 
 for user in range(NO_OF_USERS):
     user_list.append(battleShipUser(NO_OF_SHIPS))
 
 
-# for dramu in range(NO_OF_USERS):
-#     POS_INDICATOR = 10
-#     for i in range(POS_INDICATOR):
-#         print("   |", i, end="")
-#     print("   |")
-#     k = 0
-#     for i in range(ROW_SIZE):
-#         print(k, end="")
-#         for j in range(COL_SIZE):
-#             print("  |  " + str(user_list[dramu].grid[i][j]), end="")
-#         print("  |")
-#         k += 1
+# mycursor.execute(sql, val)
+# mydb.commit()
+
+
+for dramu in range(NO_OF_USERS):
+    POS_INDICATOR = 10
+    for i in range(POS_INDICATOR):
+        print("   |", i, end="")
+    print("   |")
+    k = 0
+    for i in range(ROW_SIZE):
+        print(k, end="")
+        for j in range(COL_SIZE):
+            print("  |  " + str(user_list[dramu].grid[i][j]), end="")
+        print("  |")
+        k += 1
 
 
 iterator = cycle(range(NO_OF_USERS))
