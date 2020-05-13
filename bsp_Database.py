4ebc151bcd80c9f020f5f817e1dce6d0c171d3ac
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-13 17:15:31 +0530

    Update bsp_Database.py

--- bsp_Database.py
+++ bsp_Database.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="RamuSQL#123kaka",
+    passwd="Ramukaka",
     database="mydatabase"
 )
 
