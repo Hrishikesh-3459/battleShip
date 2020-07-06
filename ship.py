8621a42fe070956be586e4250785eee2d133d75d
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-07-06 18:50:58 +0530

    Added comments

--- ship.py
+++ ship.py
@@ -1,6 +1,7 @@
 from random import randint
 import sys
 import os
+
 COL_SIZE = 10
 ROW_SIZE = 10
 SHIP_SIZE = 4
@@ -11,11 +12,13 @@ class Ship:
         self.size = SHIP_SIZE
         self.orientation = orientation
         self.sunk = False
+        
         if(self.orientation == "Vertical"):
             self.coordinates = []
             for i in range(SHIP_SIZE):
                 self.coordinates.append(
                     {'row': location['row'] + i, 'column': location['column']})
+                
         if(self.orientation == "Horizontal"):
             self.coordinates = []
             for i in range(SHIP_SIZE):
@@ -23,13 +26,15 @@ class Ship:
                     {'row': location['row'], 'column': location['column'] + i})
         self.health = ["*"] * SHIP_SIZE
 
+    # Function to check the status of each ship
     def check_health(self, sunk_ctr):
         if("*" not in self.health):
             sunk_ctr += 1
             print("Ship no. {0} sunk!".format(sunk_ctr))
             self.sunk = True
         return sunk_ctr
-
+    
+    # Function to update the status of each ship
     def change_health(self, input_cord):
         for i in range(self.size):
             if((self.coordinates[i]["row"] == input_cord['row']) and (self.coordinates[i]["column"] == input_cord['column'])):
